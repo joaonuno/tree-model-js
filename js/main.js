b00f4cce30d1b040a517235c4d4e0a58be6f1820
joaonuno <jnss81@gmail.com>
2013-06-26 09:16:27 +0100

    Added bootstrap, node animations and a walk demo.

--- js/main.js
+++ js/main.js
@@ -1,68 +1,109 @@
+var tree, root;
+
 $(function () {
   'use strict';
 
-  var tree, root, ui;
+  var COLOR_MATCH, COLOR_NO_MATCH, COLOR_VISITED, COLOR_NODE_STROKE, COLOR_TEXT_STROKE, COLOR_NODE_FILL, ui, svg;
 
+  COLOR_MATCH = '#B7FFCD';
+  COLOR_NO_MATCH = '#FFC1B7';
+  COLOR_VISITED = '#B4B4B4';
+  COLOR_NODE_STROKE = '#141414';
+  COLOR_TEXT_STROKE = '#141414';
+  COLOR_NODE_FILL = '#F1F1F1';
+
+  // Add play buttons
+  $('[data-fn]').each(function (i, el) {
+    $(el)
+      .after('<button data-play="' + $(el).data('fn') + '" class="btn btn-mini btn-primary" type="button">play</button>')
+      /*.after('<div>' +
+          '<label><input type="radio" value="depthFirstPreOrder" name="strategy"><span>depth-first pre-order</span></label>' +
+          '<label><input type="radio" value="depthFirstPostOrder" name="strategy"><span>depth-first post-order</span></label>' +
+          '<label><input type="radio" value="breadthFirst" name="strategy"><span>breadth-first</span></label>' +
+        '</div>')*/;
+  });
+  $('body').on('click', '[data-play]', function (ev) {
+    ui[$(ev.currentTarget).data('play')].fn();
+  });
+
+  // Helper function to check if a node id matches any of the given ids
   function idIn(ids) {
     return function (node) {
       return ids.indexOf(node.model.id) !== -1;
     };
   }
 
+  function getColorByMark(mark /* visited | match | noMatch */) {
+    if (mark === 'visited') return COLOR_VISITED;
+    else if (mark === 'match') return COLOR_MATCH;
+    else if (mark === 'noMatch') return COLOR_NO_MATCH;
+  }
+
+  function markNode(mark , node, i) {
+    ui.animations.push(function () {
+      node.ui.circle.transition().delay(0).duration(150).attr({
+        // fill: match ? '#58ED62' : '#F7F09E',
+        fill: getColorByMark(mark),
+        r: '20'
+      });
+      node.ui.circle.transition().delay(150).duration(250).attr({
+        r: '18'
+      });
+      svg.append('text').text(i).transition().delay(250).attr({
+        stroke: COLOR_TEXT_STROKE,
+        'font-family': 'Verdana',
+        'font-size': '10px',
+        'text-anchor': 'middle',
+        'x': 18 + parseInt(node.ui.circle.attr('cx'), 10),
+        'y': 21 + parseInt(node.ui.circle.attr('cy'), 10)
+      });
+    });
+  }
+
   // Graphical functions
   ui = {
-    markVisited: function (node) {
-      ui.animations.push(function () {
-        node.ui.circle.attr({
-          fill: 'lightgray'
-        });  
-      });
+    reset: function () {
+      if (svg) {
+        svg.remove();
+      }
+      $('#svg').append('<svg width="250" height="400"></svg>');
+      svg = d3.select('svg');
+      ui.parse.fn();
     },
-    markMatch: function (node) {
-      ui.animations.push(function () {
-        node.ui.circle.attr({
-          fill: 'orange'
-        });  
-      });
-    },
+    markVisited: markNode.bind(this, 'visited'),
+    markNoMatch: markNode.bind(this, 'noMatch'),
+    markMatch: markNode.bind(this, 'match'),
     animations: [],
     animate: function () {
       if (ui.animations.length > 0) {
         ui.currentTimeoutId = setTimeout(function () {
           ui.animations.shift()();
           ui.animate();
-        }, 500);
+        }, 400);
       }
     },
     cancelAnimations: function () {
       clearTimeout(ui.currentTimeoutId);
       ui.animations.length = 0;
-      root.walk(function (node) {
-        node.ui.circle.attr({
-          stroke: 'green',
-          fill: 'lightgreen'
-        });
-      });
     },
     parse: {
       fn: function () {
-        var RADIUS, INC_X, INC_Y, cx, cy, svg;
+        var RADIUS, INC_X, INC_Y, cx, cy;
         RADIUS = 18;
-        INC_X = RADIUS*2;
+        INC_X = RADIUS*2 + 5;
         INC_Y = RADIUS*2 + 5;
-        cx = RADIUS + 2;
-        cy = RADIUS + 2;
-        svg = d3.select('svg')
+        cx = RADIUS + 75;
+        cy = RADIUS + 5;
 
         root.walk(function (node) {
           node.ui = {
             circle: svg.append('circle').attr({
               r: RADIUS,
-              stroke: 'green',
-              fill: 'lightgreen'
+              stroke: COLOR_NODE_STROKE,
+              fill: COLOR_NODE_FILL
             }),
             label: svg.append('text').text(node.model.id).attr({
-              stroke: 'green',
+              stroke: COLOR_TEXT_STROKE,
               'font-family': 'Verdana',
               'font-size': '12px',
               'text-anchor': 'middle'
@@ -82,13 +123,15 @@ $(function () {
     },
     first: {
       fn: function () {
+        var i = 1;
+        ui.reset();
         ui.cancelAnimations();
         root.first(function (node) {
           if (node.model.id === 12) {
-            ui.markMatch(node);
+            ui.markMatch(node, i++);
             return true;
           }
-          ui.markVisited(node);  
+          ui.markNoMatch(node, i++);
           return false;
         });
         ui.animate();
@@ -96,12 +139,28 @@ $(function () {
     },
     all: {
       fn: function () {
+        var i = 1;
+        ui.reset();
         ui.cancelAnimations();
         root.walk(function (node) {
           if (node.model.id > 100) {
-            ui.markMatch(node);
+            ui.markMatch(node, i++);
           } else {
-            ui.markVisited(node);
+            ui.markNoMatch(node, i++);
+          }
+        });
+        ui.animate();
+      }
+    },
+    walk: {
+      fn: function () {
+        var i = 1;
+        ui.reset();
+        ui.cancelAnimations();
+        root.walk(function (node) {
+          ui.markVisited(node, i++);
+          if (node.model.id === 121) {
+            return false;
           }
         });
         ui.animate();
@@ -109,32 +168,23 @@ $(function () {
     }
   };
 
-  // Docs builder
-  function docFor(section) {
-    var $codeEl, code, $demoButton;
-    $codeEl = $('.code[name="' + section + '"]');
-    code = $('<div/>').html($codeEl.html()).text();
-    $demoButton = $('button[name="' + section + '"]');
-
-    $codeEl.empty();
-    CodeMirror($codeEl.get(0), {
+  $('.code').each(function (i, el) {
+    var $el, code, cm;
+    $el = $(el);
+    code = $('<div/>').html($el.html()).text();
+    $el.empty();
+    cm = CodeMirror($el.get(0), {
       value: code,
       mode: 'javascript',
-      lineNumbers: !$codeEl.is('.inline'),
+      lineNumbers: !$el.is('.inline'),
       readOnly: true
     });
-
-    eval(code);
-    if (ui[section] && ui[section].fn) {
-      if ($demoButton.length === 0) {
-        ui[section].fn();
-      } else {
-        $demoButton.on('click', ui[section].fn);
-      }
+    if (!$el.is('[data-fn]')) {
+      eval(code);
+    } else {
+      ui[$el.data('fn')].cm = cm;
     }
-  }
+  });
 
-  docFor('parse');
-  docFor('first');
-  docFor('all');
+  ui.reset();
 });
